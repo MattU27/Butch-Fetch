0f3bac4d6b9b02e14c0742587bf6806a4bab9110
nicol <angelomunoz440@gmail.com>
2024-02-28 23:36:29 +0800

    etits

--- app/src/main/java/com/example/mainmenu/MainActivity.java
+++ app/src/main/java/com/example/mainmenu/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+//test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
